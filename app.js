4540bc4a9c1f65e7c34040da45d945373e9834d9
Cintiaa <cintiasantos@alunos.utfpr.edu.br>
2019-05-08 21:02:56 -0300

    novo commit

--- app.js
+++ app.js
@@ -3,13 +3,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const logger = require('morgan');
+const path = require('path');
+const expressLayouts = require('express-ejs-layouts');
 const UsuarioRouter = require('./src/routes/usuario');
 
-//configurar dependências do projeto
+
+
+//Configurando dependências do projeto
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(logger('dev'));
+
+//Configurando mecanismo de visualização
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');    // Setamos que nossa engine será o ejs
+app.use(expressLayouts)          // Definimos que vamos utilizar o express-ejs-layouts na nossa aplicação
+
 
 //Configurando conexão com banco de dados
 mongoose.connect('mongodb://agenda:agenda123@ds151086.mlab.com:51086/db-agenda', {useNewUrlParser: true})
@@ -24,19 +34,22 @@ mongoose.connect('mongodb://agenda:agenda123@ds151086.mlab.com:51086/db-agenda',
 //Configurando porta
 const porta = process.env.port || 3000;
 
-//Configurando mecanismo de visualização
-app.set('views', __dirname + './views');
-app.set('view engine', 'ejs');
-
 
 //Configurando rotas
-app.get('/', (req, res) =>{
-    res.status(200).json({
-        message: 'Bem vindo a agenda comunitária',
-    });
+ app.get('/', (req, res) =>{
+    res.render('index');
 });
 
+/* app.get('/', (req, res) => {
+    res.render('index');
+});
+   */
+
 app.use('/', UsuarioRouter);
+
+
+
+
 
 app.listen(porta, () =>{
     console.log(`Servidor rodando na porta ${porta}`);
--- src/routes/usuario.js
+++ src/routes/usuario.js
@@ -4,10 +4,6 @@ const router = express.Router();
 
 const usuarioController = require('../controllers/usuario');
 
-//Request para acessar a View
-router.get('/', function (req, res) {
-    res.render('index');
-});
 
 //Request para criar um novo usuário
 router.post('/usuario', usuarioController.novoUsuario);
